e21a5b6dd68fad98e4d1118ff1d9cdea96d16cb5
Adam Vitti <advi4972@colorado.edu>
2021-01-31 14:18:25 -0700

    Added Lab 2 code (LED blink)

--- Bivalvia_Lab1/main.c
+++ Bivalvia_Lab1/main.c
@@ -3,9 +3,13 @@
 
 /**
  * main.c
- */
+**/
+
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
-	//HOla team my names adam vitti how are yall doing
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
+
+    P1->DIR |= BIT0;
+    P1->OUT &= ~BIT1;
+
 }
