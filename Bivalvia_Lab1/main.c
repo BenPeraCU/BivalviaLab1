6a6ce7e902989c7790312874add61518d1936ddb
Adam Vitti <advi4972@colorado.edu>
2021-01-31 14:10:52 -0700

    Heylllo - Adam V.

--- Bivalvia_Lab1/main.c
+++ Bivalvia_Lab1/main.c
@@ -7,5 +7,5 @@
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
-	//HOla team my names adam vitti how are yall doin
+	//HOla team my names adam vitti how are yall doing
 }
