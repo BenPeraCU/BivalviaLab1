5f85f7ce68fe7bbb164af1f0bec1c54728d504d8
BenPeraCU <73373478+BenPeraCU@users.noreply.github.com>
2021-02-02 19:25:02 -0700

    scanMemory

--- Bivalvia_Lab1/Release/subdir_rules.mk
+++ Bivalvia_Lab1/Release/subdir_rules.mk
@@ -2,11 +2,13 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
+SHELL = cmd.exe
+
 # Each subdirectory must supply rules for building sources it contributes
 %.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me -O2 --include_path="/Applications/ti/ccs1020/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1020/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/adamvitti/git/BivalviaLab1/Bivalvia_Lab1" --include_path="/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me -O2 --include_path="C:/ti/ccs1020/ccs/ccs_base/arm/include" --include_path="C:/ti/ccs1020/ccs/ccs_base/arm/include/CMSIS" --include_path="C:/Users/Benja/git/BivalviaLab1/Bivalvia_Lab1" --include_path="C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
