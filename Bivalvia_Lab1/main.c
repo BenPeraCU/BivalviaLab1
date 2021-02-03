4b5a4a5a00042743caa6955e0a39f2b4a2304fef
BenPeraCU <73373478+BenPeraCU@users.noreply.github.com>
2021-02-03 13:27:52 -0700

    After Lab

--- Bivalvia_Lab1/main.c
+++ Bivalvia_Lab1/main.c
@@ -4,14 +4,21 @@
  * main.c
 **/
 
+uint16_t *ptr;
+
 void main(void)
 {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
 
+    ptr = (uint16_t *)0x20000000U;
+
+    *(ptr + 0x10) = 0xACCA;
+    *(ptr + 0x11) = 0xACCA;
+    *(ptr + 0x12) = 0xADDA;
+
     uint32_t count1 = 0;
     uint32_t count2 = 0;
 
-    uint32_t *ptr = (uint32_t *)0x2001000A;
     for(ptr; ptr < (uint32_t *)0x200103CD; ptr++){
         if(*ptr == 0x0000ACCA){
             count1++;
@@ -20,5 +27,4 @@ void main(void)
             count2++;
         }
     }
-
 }
