82a6c68b053cec8a6544494cce7e754aa244460e
Adam Vitti <advi4972@colorado.edu>
2021-02-02 18:18:20 -0700

    Finished??

--- Bivalvia_Lab1/main.c
+++ Bivalvia_Lab1/main.c
@@ -10,10 +10,13 @@ void main(void)
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
 
     P1->DIR |= BIT0; //sets p1.0 as output
-    P1->OUT &= ~BIT1; // sets out to 0
+    P1->OUT &= ~BIT1; // sets bit1 to 0
 
     while(1){
         P1->OUT |= BIT0;
+        for(int i = 0; i < 30000; i++);
+        P1->OUT &= ~BIT0;
+        for(int i = 0; i < 30000; i++);
     }
 
 }
