ad37bc4024903c2ac72285cbc6cb48c398d38b7a
BenPeraCU <73373478+BenPeraCU@users.noreply.github.com>
2021-02-02 22:58:41 -0700

    Still buggy

--- Bivalvia_Lab1/main.c
+++ Bivalvia_Lab1/main.c
@@ -11,13 +11,13 @@ void main(void)
     uint32_t count1 = 0;
     uint32_t count2 = 0;
 
-    uint32_t *ptr = (uint32_t *)0x200100A;
+    uint32_t *ptr = (uint32_t *)0x2001000A;
     for(ptr; ptr < (uint32_t *)0x200103CD; ptr++){
-        if(*ptr == 0xABBAAFFA){
-            count2++;
-        }
         if(*ptr == 0x0000ACCA){
             count1++;
+        }
+        if(*ptr == 0xABBAAFFA){
+            count2++;
         }
     }
 
