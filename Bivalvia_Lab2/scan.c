9269f4860cf43bdda548879155d509c26940bccf
notmike5 <>
2021-02-03 13:10:03 -0700

    scan.c

--- Bivalvia_Lab2/scan.c
+++ Bivalvia_Lab2/scan.c
@@ -1,12 +1,11 @@
 //scan.c
 
-
 void main(int argsc, char *argsv[]) {
 
     int a = 0, b = 0;
     short int *start = (short int*)0x2001000A, *end = (short int *)0x200103CC;
 
-    while(*start < *end) {
+    while(start <= end) {
         if((*start) == 0xACCA) { a++; }
         if((*start) == 0xABBA) && (*start + 1) == 0xAFFA) { b++; }
         start++;
