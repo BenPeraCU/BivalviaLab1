b959aa5134dad7ae53499d76a9e37a70bc204391
Adam Vitti <advi4972@colorado.edu>
2021-02-02 18:40:28 -0700

    Blink LED - Adam

--- Bivalvia_Lab1/main.c
+++ Bivalvia_Lab1/main.c
@@ -10,13 +10,18 @@ void main(void)
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
 
     P1->DIR |= BIT0; //sets p1.0 as output
-    P1->OUT &= ~BIT1; // sets bit1 to 0
+    P1->OUT &= ~BIT0; // sets bit0 to 0
 
     while(1){
-        P1->OUT |= BIT0;
-        for(int i = 0; i < 30000; i++);
-        P1->OUT &= ~BIT0;
-        for(int i = 0; i < 30000; i++);
+        int i;
+        P1->OUT |= BIT0; //Sets LED to HIGH (pin1.out = 1b)
+        for(i = 0; i < 100000; i++); //Delays code
+        P1->OUT &= ~BIT0; //Sets LED to LOW (pin1.out = 0b)
+        for(i = 0; i < 100000; i++); //Delays code
+        P1->OUT |= BIT0; //Sets LED to HIGH (pin1.out = 1b)
+        for(i = 0; i < 100000; i++); //Delays code
+        P1->OUT &= ~BIT0; //Sets LED to LOW (pin1.out = 0b)
+        for(i = 0; i < 100000; i++); //Delays code
     }
 
 }
